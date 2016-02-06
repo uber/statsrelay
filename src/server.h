d3491121c4155b400b7cec2a7921d76246f13756
Yash <yash@lyft.com>
2016-02-05 16:34:02 -0800

    Clean up the header

--- src/server.h
+++ src/server.h
@@ -30,11 +30,6 @@ bool connect_server_collection(struct server_collection *server_collection,
 
 void destroy_server_collection(struct server_collection *server_collection);
 
-/**
-  * TODO: combine the below two functions
-  */
 void stop_accepting_connections(struct server_collection *server_collection);
-
-void flush_and_kill_clients(struct server_collection *server_collection);
 
 #endif  // STATSRELAY_SERVER_H
