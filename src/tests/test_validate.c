c8d6cc0155bc53eafedaafe896c55c88b12a831a
Charlie Vieth <cvieth@lyft.com>
2019-06-12 12:06:43 -0400

    validate: don't modify input and remove alloc (#99)
    Merge changes from: https://github.com/lyft/statsrelay/pull/35
    
    Changes to: `validate_statsd()`:
    * Respect that `const char *line` is `const` (i.e. don't modify it, which causes the `const` qualifier to be dropped)
    * Remove unnecessary duplication of the line argument
    * Uses `consts` for valid_stat_types array
    * Move stat type parsing to `parse_stat_type`: nit, this can be further improved with a tool like `gperf`.

--- src/tests/test_validate.c
+++ src/tests/test_validate.c
@@ -3,16 +3,89 @@
 #include <assert.h>
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "../log.h"
 #include "../filter.h"
 #include "../validate.h"
 
+static void test_line_not_modified(filter_t* filter) {
+    const char *lines[] = {
+        "a.b.c.__tag1=v1.__tag2=v2:v2:42.000|ms",
+        "test.srv.req:2.5|ms|@0.2",
+    };
+    const int lines_len = sizeof(lines) / sizeof(char *);
+
+    for (int i = 0; i < lines_len; i++) {
+        const char *line = lines[i];
+        const char *copy = strdup(line);
+
+        validate_parsed_result_t result;
+        assert(validate_statsd(line, strlen(line), &result, filter, true) == 0);
+        assert(strcmp(line, copy) == 0);
+    }
+}
+
+static void test_parse_presampling_value(filter_t* filter) {
+    validate_parsed_result_t result;
+
+    static const char* exp1 = "test.srv.req:2.5|ms|@0.2";
+
+    assert(0 == validate_statsd(exp1, strlen(exp1), &result, filter, true));
+    assert(2.5 == result.value);
+    assert(0.2 == result.presampling_value);
+    assert(METRIC_TIMER == result.type);
+}
+
+typedef struct {
+    const char  *stat;
+    metric_type expected;
+} stat_type_test;
+
+static const stat_type_test stat_type_tests[] = {
+    {"c", METRIC_COUNTER},
+    {"ms", METRIC_TIMER},
+    {"kv", METRIC_KV},
+    {"g", METRIC_GAUGE},
+    {"h", METRIC_HIST},
+    {"s", METRIC_S},
+    {"", METRIC_UNKNOWN},
+    {"a", METRIC_UNKNOWN},
+    {"b", METRIC_UNKNOWN},
+    {"abc", METRIC_UNKNOWN},
+    {" ", METRIC_UNKNOWN},
+    {"ss", METRIC_UNKNOWN},
+    {"m", METRIC_UNKNOWN},
+    {"cc", METRIC_UNKNOWN},
+};
+static const int stat_type_tests_len = sizeof(stat_type_test) / sizeof(stat_type_tests[0]);
+
+static void test_parse_stat_type(filter_t* filter) {
+    char buf[256];
+    char *line = &buf[0];
+
+    validate_parsed_result_t result;
+    stat_type_test test;
+    int ret;
+
+    for (int i = 0; i < stat_type_tests_len; i++) {
+        test = stat_type_tests[i];
+        assert(snprintf(line, sizeof(buf), "a.b.c:3|%s", test.stat) > 0);
+        ret = validate_statsd(line, strlen(line), &result, filter, true);
+        if (test.expected == METRIC_UNKNOWN) {
+            assert(ret != 0); // invalid
+        } else {
+            assert(ret == 0); // valid
+        }
+        assert(result.type == test.expected);
+    }
+}
 
 int main(int argc, char **argv) {
     stats_log_verbose(1);
 
     filter_t* filter;
+
     validate_parsed_result_t result;
     char *point_tags_regex = "\\.__([a-zA-Z][a-zA-Z0-9_]+)=[^.]+";
 
@@ -66,9 +139,12 @@ int main(int argc, char **argv) {
     static const char *bad5 = "a.b.c.__tag1=v1.__tag2=v2:v2:NAN|ms";
     assert(1 == validate_statsd(bad5, strlen(bad5), &result, filter, true));
 
+    // additional tests
+    test_line_not_modified(filter);
+    test_parse_presampling_value(filter);
+    test_parse_stat_type(filter);
+
     // free the filter
     // to only log warning and not reject the line
     filter_free(filter);
-
-    return 0;
 }
--- src/validate.c
+++ src/validate.c
@@ -4,7 +4,7 @@
 #include "log.h"
 #include "validate.h"
 
-static const char *valid_stat_types[6] = {
+static const char * const valid_stat_types[6] = {
     "c",
     "ms",
     "kv",
@@ -12,8 +12,9 @@ static const char *valid_stat_types[6] = {
     "h",
     "s"
 };
+static const int valid_stat_types_len = 6;
 
-static const char *reserved_tag_names[10] = {
+static const char * const reserved_tag_names[10] = {
         "asg",
         "az",
         "backend",
@@ -25,113 +26,97 @@ static const char *reserved_tag_names[10] = {
         "source",
         "window"
 };
-static size_t valid_stat_types_len = 6;
-static size_t reserved_tag_names_len = 10;
+static const int reserved_tag_names_len = 10;
 
 extern void *memrchr(const void*, int, size_t);
 
+static metric_type parse_stat_type(const char *str, size_t len) {
+    if (1 <= len && len <= 2) {
+        for (int i = 0; i < valid_stat_types_len; i++) {
+            const char *stat = valid_stat_types[i];
+            // len is bounded, so help the the compiler optimize
+            if ((len == 1 ? memcmp(stat, str, 1) : memcmp(stat, str, 2)) == 0) {
+                return (metric_type)i;
+            }
+        }
+    }
+    return METRIC_UNKNOWN;
+}
 
 int validate_statsd(const char* line, size_t len, validate_parsed_result_t* result, filter_t* point_tag_validator,
                     bool validate_point_tags) {
-    size_t plen;
-    char c;
-    int* ovector, i, valid, offset, rc;
+
+    int* ovector, offset, rc;
     bool reserved_tagname = false;
 
-    // FIXME: this is dumb, don't do a memory copy
-    char *line_copy = strndup(line, len);
-    char *start, *end;
-    char *err;
+    const char *start = line;
+    size_t plen = len;
 
-    if (line_copy == NULL) {
-        stats_log("validate: allocation failure. marking invalid line");
-        goto statsd_err;
-    }
-
-    start = line_copy;
-    plen = len;
+    // Search backwards, otherwise might eat up irrelevant data.
     // Example: keyname.__tagname=tag:value:42.0|ms
-    //                                      ^^^^--- actual value  
-    end = memrchr(start, ':', plen);
+    //                                      ^^^^--- actual value
+    const char *end = memrchr(start, ':', plen);
     if (end == NULL) {
         stats_log("validate: Invalid line \"%.*s\" missing ':'", len, line);
-        goto statsd_err;
+        return 1;
     }
-
     if ((end - start) < 1) {
         stats_log("validate: Invalid line \"%.*s\" zero length key", len, line);
-        goto statsd_err;
+        return 1;
     }
-
     start = end + 1;
-    plen = len - (start - line_copy);
+    plen = len - (start - line);
 
-    c = end[0];
-    end[0] = '\0';
-    result->presampling_value = 1.0; /* Default pre-sampling to 1.0 */
+    char *err;
     result->value = strtod(start, &err);
-    if ((result->value == 0.0) && (err == start)) {
+    if (result->value == 0 && err == start) {
         stats_log("validate: Invalid line \"%.*s\" unable to parse value as double", len, line);
-        goto statsd_err;
+        return 1;
     }
     if (isnan(result->value) || isinf(result->value)) {
         stats_log("validate: Invalid value \"%.*s\" supplied (NaN or INF)", len, line);
-        goto statsd_err;
+        return 1;
     }
-    end[0] = c;
 
     end = memchr(start, '|', plen);
     if (end == NULL) {
         stats_log("validate: Invalid line \"%.*s\" missing '|'", len, line);
-        goto statsd_err;
+        return 1;
     }
-
     start = end + 1;
-    plen = len - (start - line_copy);
+    plen = len - (start - line);
 
     end = memchr(start, '|', plen);
     if (end != NULL) {
-        c = end[0];
-        end[0] = '\0';
         plen = end - start;
     }
 
-    valid = 0;
-    for (i = 0; i < valid_stat_types_len; i++) {
-        if (strlen(valid_stat_types[i]) != plen) {
-            continue;
-        }
-        if (strncmp(start, valid_stat_types[i], plen) == 0) {
-            result->type = (metric_type)i; /* The indexes match the enum values in the comparison list */
-            valid = 1;
-            break;
-        }
+    result->type = parse_stat_type(start, plen);
+    if (result->type < 0) {
+        stats_log("validate: Invalid line \"%.*s\" unknown stat type \"%.*s\"", len, line, plen, start);
+        return 1;
     }
 
-    if (valid == 0) {
-        stats_log("validate: Invalid line \"%.*s\" unknown stat type \"%.*s\"", len, line, plen, start);
-        goto statsd_err;
-    }
+    result->presampling_value = 1.0; /* Default pre-sampling to 1.0 */
 
     if (end != NULL) {
-        end[0] = c;
         // end[0] is currently the second | char
         // test if we have at least 1 char following it (@)
-        if ((len - (end - line_copy) > 1) && (end[1] == '@')) {
+        if ((len - (end - line) > 1) && (end[1] == '@')) {
             start = end + 2;
-            plen = len - (start - line_copy);
+            plen = len - (start - line);
             if (plen == 0) {
                 stats_log("validate: Invalid line \"%.*s\" @ sample with no rate", len, line);
-                goto statsd_err;
+                return 1;
             }
             result->presampling_value = strtod(start, &err);
             if ((result->presampling_value == 0.0) && err == start) {
                 stats_log("validate: Invalid line \"%.*s\" invalid sample rate", len, line);
-                goto statsd_err;
+                return 1;
             }
         } else {
             stats_log("validate: Invalid line \"%.*s\" no @ sample rate specifier", len, line);
-            goto statsd_err;
+            return 1;
         }
     }
 
@@ -152,13 +137,8 @@ int validate_statsd(const char* line, size_t len, validate_parsed_result_t* resu
     free(ovector);
 
     if (reserved_tagname && validate_point_tags) {
-        goto statsd_err;
+        return 1;
     }
 
-    free(line_copy);
     return 0;
-
-statsd_err:
-    free(line_copy);
-    return 1;
 }
